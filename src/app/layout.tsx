a1b8c90c66f3440b38c91f227f261df074940ac2
dipu singh <dipusingh8294935492@gmail.com>
2024-11-13 05:11:33 +0530

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import ScrollButton from "@/components/ScrollButton";
+import ScrollButton from "@/components/Scrollbutton";
 
 const inter = Inter({ subsets: ["latin"] });
 
